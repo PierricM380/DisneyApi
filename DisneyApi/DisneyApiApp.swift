4dc32ca15b070060f04bb777529ccae7b9d8b488
PierricM380 <pierric.marye@wanadoo.fr>
2023-01-05 10:34:28 +0100

    Initial Commit

---
+++ DisneyApi/DisneyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyApiApp.swift
+//  DisneyApi
+//
+//  Created by Pierric Marye on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DisneyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
